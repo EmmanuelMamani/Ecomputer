13107fbf312a7662fd6f9647ed1f755491287c33
emmanuelmamani <emmanuelmamani100@gmail.com>
2022-11-01 22:08:59 -0400

    Empezamos

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import encabezadoVue from './components/encabezado.vue';
+</script>
+
+<template>
+<encabezadoVue></encabezadoVue>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/encabezado.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+<template>
+    <div class="row" id="encabezado">
+        <span class="col-3">Ecomputer</span>
+        <div class="col-6">
+            <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="¿Que quieres buscar?" aria-label="Search">
+            <button class="btn btn-outline-light" type="submit">Buscar</button>
+            </form>
+        </div>
+        <div class="col item"><span class="material-symbols-outlined">shopping_cart</span><span id="monto">0.00 Bs</span></div>
+        <div class="col item"><span class="material-symbols-outlined">person </span><span id="usuario">Iniciar Sesión</span></div>
+    </div>
+</template>
+<style scoped>
+    #encabezado{
+        background-color: rgb(55, 55, 55);
+        color: white;
+        font-size: 20px;
+        padding: 2%;
+    }
+    #monto,#usuario{
+        font-size: 18px;
+    }
+    .item{
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+    }
+    .item:hover{
+        border: 1px white solid;
+        border-radius: 5px;
+    }
+</style>
